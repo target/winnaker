4571477ca45eadd6bf4ff10ac0afa57622396bb7
Edwin Avalos <edwin.avalos@target.com>
2017-06-14 15:35:56 -0500

    Adding get-env to helpers for sane defaults
    * This makes it so you don't need to necessarily add the XPATH overrides
    We could also move those out of the sample env, but I chose to leave them
    
    * Adding a test for the get_env function in helpers_test.py

---
+++ winnaker/helpers_test.py
@@ -0,0 +1,12 @@
+import os, random, string
+from helpers import get_env
+
+def test_get_env():
+    random_string = ''.join(random.choice(string.lowercase) for i in range(15))
+    print "Random String: {}".format(random_string)
+    os.environ["WINNAKER_TEST_ENV_VAR"] = "testingtesting"
+    assert get_env("WINNAKER_TEST_ENV_VAR", "nottherightthing") == "testingtesting"
+    assert get_env(random_string, "thedefault") == "thedefault"
+    return True
+
+assert test_get_env() == True
